e0e602e0cfa6018a2011ea2f60f5ffafe6ca1dbd
Tadasls <tadas.laurinaitis@gmail.com>
2023-01-17 12:43:34 +0200

    addedconds to function

--- Js/timer.js
+++ Js/timer.js
@@ -8,12 +8,14 @@ document.addEventListener("DOMContentLoaded", () => {
     window.location.href = "login.html";}
 
     const rol = Object.assign({}, JSON.parse(localStorage.getItem('role')));
-    console.log(rol.role)
+   // console.log(rol.role)
     if (rol.role!=`admin`) {
-      console.log(rol.role)
+   //   console.log(rol.role)
         alert('No Access. Just For Admins');
         window.location.href = "todo.html";}
 });
+
+
 
 
 var milliseconds = 0;
